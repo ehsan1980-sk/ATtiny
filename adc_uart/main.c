e8d5b7cffc8ae5803635f8ad273382c939d59657
Kenta Ishii <k.i.drivingroad21@gmail.com>
2019-12-02 21:46:39 +0900

    Modified Noise Generator, Fixed High State on Noise Off to Low State
    Added ADC UART

---
+++ adc_uart/main.c
@@ -0,0 +1,126 @@
+/**
+ * main.c
+ *
+ * Author: Kenta Ishii
+ * License: 3-Clause BSD License
+ * License URL: https://opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
+#define F_CPU 9600000UL // Default 9.6Mhz to ATtiny13
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/cpufunc.h>
+#include <util/delay.h>
+#include <util/delay_basic.h>
+
+void software_uart_init( uint8_t portb_pin_number_for_tx );
+void software_uart_print_9600( char* string, uint16_t length, uint8_t portb_pin_number_for_tx );
+void software_uart_tx_9600( uint8_t character, uint8_t portb_pin_number_for_tx );
+uint8_t software_uart_tx_pin;
+
+int main(void) {
+	uint8_t select_adc_channel_2 = _BV(MUX1); // ADC2 (PB4)
+	uint8_t clear_adc_channel = ~(_BV(MUX1)|_BV(MUX0));
+	uint8_t start_adc = _BV(ADSC);
+	uint16_t value_adc;
+
+	PORTB = 0; // All Low
+	DDRB = 0; // All Input
+
+	software_uart_init( 3 );
+
+	// Set ADC, Vcc as Reference, No ADLAR
+	ADMUX = 0;
+	// Enable ADC, Prescaler 64 to Have ADC Clock 150Khz.
+	ADCSRA = _BV(ADEN)|_BV(ADPS2)|_BV(ADPS1);
+
+	while(1) {
+		ADMUX |= select_adc_channel_2;
+		ADCSRA |= start_adc;
+		while( ADCSRA & start_adc );
+		ADMUX &= clear_adc_channel;
+		value_adc = ADCH<<8|ADCL; // Read Low Bits First
+		value_adc |= ADCH<<8; // ADC[9:0] Will Be Updated After High Bits Are Read.
+
+		software_uart_tx_9600( (uint8_t)value_adc, software_uart_tx_pin );
+		software_uart_tx_9600( (uint8_t)value_adc>>8, software_uart_tx_pin );
+		_delay_ms( 500 );
+	}
+	return 0;
+}
+
+void software_uart_init( uint8_t portb_pin_number_for_tx ) {
+	PORTB |= _BV( portb_pin_number_for_tx );
+	DDRB |= _BV( portb_pin_number_for_tx );
+	_NOP();
+	software_uart_tx_pin = portb_pin_number_for_tx;
+}
+
+void software_uart_print_9600( char* string, uint16_t length, uint8_t portb_pin_number_for_tx ) {
+	for( uint16_t i = 0; i < length; i++ ) software_uart_tx_9600( string[i], portb_pin_number_for_tx );
+}
+
+void software_uart_tx_9600( uint8_t character, uint8_t portb_pin_number_for_tx ) {
+	/**
+	 * First Argument is r24, Second Argument is r22 (r25 to r8, Assign Even Number Registers)
+	 * r18-r27, r30-r31 are scratch registers.
+	 * r2-r17, r28-r29 need push/pop in the function.
+	 * r0 is temporary and r1 is zero.
+	 */
+	register const uint8_t count_delay asm("r18") = 247; // 247 * 4, 988 Clocks
+	register uint8_t count asm("r19");
+	register uint8_t set_bit_pin asm("r20") = _BV( portb_pin_number_for_tx );
+	register uint8_t clear_bit_pin asm("r21") = ~( set_bit_pin );;
+	register uint8_t i asm("r22") = 9;
+	asm volatile (
+			"clc" "\n\t" // Clear Carry Bit for Start Bit
+		"software_uart_tx_9600_forloop:" "\n\t" // 12 Clocks + Delay 988 Clocks
+			"in __tmp_reg__, %[portb]" "\n\t"
+			"brcc software_uart_tx_9600_forloop_carryclear" "\n\t" // From Result of lsr
+			"or __tmp_reg__, %[set_bit_pin]" "\n\t"
+			"rjmp software_uart_tx_9600_forloop_common" "\n\t"
+			"software_uart_tx_9600_forloop_carryclear:" "\n\t"
+				"and __tmp_reg__, %[clear_bit_pin]" "\n\t"
+				"nop" "\n\t"
+			"software_uart_tx_9600_forloop_common:" "\n\t"
+				"out %[portb], __tmp_reg__" "\n\t"
+				// count_delay * 4 Cycles
+				"mov %[count], %[count_delay]" "\n\t"
+				"software_uart_tx_9600_forloop_common_delay:" "\n\t"
+					"nop" "\n\t"
+					"subi %[count], 0x1" "\n\t"
+					"brne software_uart_tx_9600_forloop_common_delay" "\n\t"
+				"subi %[i], 0x1" "\n\t"
+				"breq software_uart_tx_9600_stopbit" "\n\t" // Branch If Lower
+				"nop" "\n\t"
+				"lsr %[character]" "\n\t"
+				"rjmp software_uart_tx_9600_forloop" "\n\t"
+		"software_uart_tx_9600_stopbit:" "\n\t"
+			"in __tmp_reg__, %[portb]" "\n\t"
+			"or __tmp_reg__, %[set_bit_pin]" "\n\t"
+			"rjmp .+0" "\n\t" // Two Cycles
+			"rjmp .+0" "\n\t" // Two Cycles
+			"rjmp .+0" "\n\t" // Two Cycles
+			"out %[portb], __tmp_reg__" "\n\t"
+			//count_delay * 4 Cycles
+			"mov %[count], %[count_delay]" "\n\t"
+			"inc %[count]" "\n\t"
+			"software_uart_tx_9600_stopbit_delay:" "\n\t"
+				"nop" "\n\t"
+				"subi %[count], 0x1" "\n\t"
+				"brne software_uart_tx_9600_stopbit_delay" "\n\t"
+				"rjmp .+0" "\n\t" // Two Cycles
+		/* Outputs */
+		:[count]"=d"(count),
+		 [i]"+d"(i)
+		/* Inputs */
+		:[portb]"I"(_SFR_IO_ADDR(PORTB)),
+		 [set_bit_pin]"r"(set_bit_pin),
+		 [clear_bit_pin]"r"(clear_bit_pin),
+		 [count_delay]"r"(count_delay),
+		 [character]"r"(character)
+		/* Clobber List */
+		:
+	);
+}
--- noise_generator/main.c
+++ noise_generator/main.c
@@ -17,21 +17,25 @@
 int main(void) {
 	uint16_t random_value = 0xFFFF; // Non-zero Value
 	uint8_t pin_button = _BV(PINB4); // Assign PB4 as Button Input
+	uint8_t pwm_start = _BV(CS00); // I/O-Clock 9.6MHz / ( 1 * 256 ) = 37500Hz
+	uint8_t pwm_stop = ~(_BV(CS00));
+	uint8_t pwm_clear = ~(_BV(PB1)); // PB1 (OC0B) Low
+	uint8_t output_start = _BV(DDB1); // Bit Value Set PB1 (OC0B) as Output
+	uint8_t output_stop = ~(_BV(DDB1)); // Bit Value Clear PB1(OC0B)
 
 	PORTB = 0; // All Low
 	DDRB = 0; // All Input
 	PORTB |= _BV(PB4); // Pullup Button Input (There is No Internal Pulldown)
-	DDRB |= _BV(DDB1); // Bit Value Set PB1 (OC0B) as Output
 	DDRB &= ~(_BV(DDB4)); // Bit Value Clear PB4 as Button Input
 	_NOP(); // Wait for Synchronization
 
 	// Counter Reset
 	TCNT0 = 0;
 
-	// Select Fast PWM Mode and Output on OC0B
+	// Select Fast PWM Mode (3) and Output on OC0B
+	// Fast PWM Mode (7) can make variable frequencies with adjustable duty cycle by settting OCR0A as TOP, but OC0B is only available.
 	TCCR0A = _BV(COM0B1)|_BV(WGM01)|_BV(WGM00);
-	// Stop Counter
-	TCCR0B = 0;
+	TCCR0B = 0; // Initialize
 
 	while(1) {
 		if ( ! (PINB & pin_button) ) { // Output Noise
@@ -39,15 +43,21 @@ int main(void) {
 			random_value = rand();
 			OCR0B = random_value;
 			// Start Output
-			if ( ! TCCR0B ) {
-				// Start Counter with I/O-Clock, 9.6MHz / ( 1 * 256 ) = 37500Hz
-				TCCR0B = _BV(CS00);
+			if ( ! ( TCCR0B & pwm_start ) ) {
+				// Bit Value Set PB1 (OC0B) as Output
+				DDRB |= output_start;
+				// Start Counter
+				TCCR0B |= pwm_start;
 			}
 		} else { // No Output
 			// Stop Output
-			if ( TCCR0B ) {
+			if ( TCCR0B & pwm_start ) {
 				// Stop Counter
-				TCCR0B = 0;
+				TCCR0B &= pwm_stop;
+				// PB1 (OC0B) Low
+				PORTB &= pwm_clear;
+				// Bit Value Clear PB1(OC0B)
+				DDRB &= output_stop;
 			}
 		}
 	}

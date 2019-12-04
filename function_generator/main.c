6d9a317150aff01e42504e51fe36f9d88197917d
Kenta Ishii <k.i.drivingroad21@gmail.com>
2019-12-04 10:13:28 +0900

    Modified Noise Generator about Noise On/Off
    Added Function Generator

---
+++ function_generator/main.c
@@ -0,0 +1,76 @@
+/**
+ * main.c
+ *
+ * Author: Kenta Ishii
+ * License: 3-Clause BSD License
+ * License URL: https://opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
+#define F_CPU 9600000UL // Default 9.6Mhz to ATtiny13
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/cpufunc.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <util/delay_basic.h>
+
+int main(void) {
+	PORTB = 0; // All Low
+	DDRB = 0; // All Input
+	PORTB |= _BV(PB4); // Pullup Button Input (There is No Internal Pulldown)
+	DDRB &= ~(_BV(DDB4)); // Bit Value Clear PB4 as Button Input
+	_NOP(); // Wait for Synchronization
+
+	// Counter Reset
+	TCNT0 = 0;
+
+	// Set Output Compare B
+	OCR0B = 128;
+
+	// Set Timer/Counter0 Overflow Interrupt for "ISR(TIM0_OVF_vect)"
+	TIMSK0 = _BV(TOIE0);
+
+	// Select Fast PWM Mode (3)
+	// Fast PWM Mode (7) can make variable frequencies with adjustable duty cycle by settting OCR0A as TOP, but OC0B is only available.
+	TCCR0A = _BV(WGM01)|_BV(WGM00);
+
+	// Start Counter with I/O-Clock 9.6MHz / ( 256 * 256 ) = 146.484375Hz
+	TCCR0B = _BV(CS02);
+
+	// Start to Issue Interrupt
+	sei(); // cli() for Stop
+
+	while(1) {
+	}
+	return 0;
+}
+
+ISR(TIM0_OVF_vect) {
+	uint8_t const pin_button = _BV(PINB4); // Assign PB4 as Button Input
+	uint8_t const pwm_output_b_start = _BV(COM0B1); // Non-inverted
+	uint8_t const pwm_output_b_stop = ~(_BV(COM0B1)|_BV(COM0B0));
+	uint8_t const output_clear = ~(_BV(PB1)); // PB1 (OC0B) Low
+	uint8_t const output_start = _BV(DDB1); // Bit Value Set PB1 (OC0B) as Output
+	uint8_t const output_stop = ~(_BV(DDB1)); // Bit Value Clear PB1(OC0B)
+
+	if ( ! (PINB & pin_button) ) { // Output Noise
+		// Start Output
+		if ( ! ( DDRB & output_start ) ) {
+			// PWM Output Start
+			TCCR0A |= pwm_output_b_start;
+			// Bit Value Set PB1 (OC0B) as Output
+			DDRB |= output_start;
+		}
+	} else { // No Output
+		// Stop Output
+		if ( DDRB & output_start ) {
+			// PWM Output Stop
+			TCCR0A &= pwm_output_b_stop;
+			// PB1 (OC0B) Low
+			PORTB &= output_clear;
+			// Bit Value Clear PB1(OC0B), High-Z State
+			DDRB &= output_stop;
+		}
+	}
+}
--- noise_generator/main.c
+++ noise_generator/main.c
@@ -16,12 +16,12 @@
 
 int main(void) {
 	uint16_t random_value = 0xFFFF; // Non-zero Value
-	uint8_t pin_button = _BV(PINB4); // Assign PB4 as Button Input
-	uint8_t pwm_start = _BV(CS00); // I/O-Clock 9.6MHz / ( 1 * 256 ) = 37500Hz
-	uint8_t pwm_stop = ~(_BV(CS00));
-	uint8_t pwm_clear = ~(_BV(PB1)); // PB1 (OC0B) Low
-	uint8_t output_start = _BV(DDB1); // Bit Value Set PB1 (OC0B) as Output
-	uint8_t output_stop = ~(_BV(DDB1)); // Bit Value Clear PB1(OC0B)
+	uint8_t const pin_button = _BV(PINB4); // Assign PB4 as Button Input
+	uint8_t const pwm_output_b_start = _BV(COM0B1); // Non-inverted
+	uint8_t const pwm_output_b_stop = ~(_BV(COM0B1)|_BV(COM0B0));
+	uint8_t const output_clear = ~(_BV(PB1)); // PB1 (OC0B) Low
+	uint8_t const output_start = _BV(DDB1); // Bit Value Set PB1 (OC0B) as Output
+	uint8_t const output_stop = ~(_BV(DDB1)); // Bit Value Clear PB1(OC0B)
 
 	PORTB = 0; // All Low
 	DDRB = 0; // All Input
@@ -32,10 +32,12 @@ int main(void) {
 	// Counter Reset
 	TCNT0 = 0;
 
-	// Select Fast PWM Mode (3) and Output on OC0B
+	// Select Fast PWM Mode (3)
 	// Fast PWM Mode (7) can make variable frequencies with adjustable duty cycle by settting OCR0A as TOP, but OC0B is only available.
-	TCCR0A = _BV(COM0B1)|_BV(WGM01)|_BV(WGM00);
-	TCCR0B = 0; // Initialize
+	TCCR0A = _BV(WGM01)|_BV(WGM00);
+
+	// Start Counter with I/O-Clock 9.6MHz / ( 1 * 256 ) = 37500Hz
+	TCCR0B = _BV(CS00);
 
 	while(1) {
 		if ( ! (PINB & pin_button) ) { // Output Noise
@@ -43,19 +45,19 @@ int main(void) {
 			random_value = rand();
 			OCR0B = random_value;
 			// Start Output
-			if ( ! ( TCCR0B & pwm_start ) ) {
+			if ( ! ( DDRB & output_start ) ) {
+				// PWM Output Start
+				TCCR0A |= pwm_output_b_start;
 				// Bit Value Set PB1 (OC0B) as Output
 				DDRB |= output_start;
-				// Start Counter
-				TCCR0B |= pwm_start;
 			}
 		} else { // No Output
 			// Stop Output
-			if ( TCCR0B & pwm_start ) {
-				// Stop Counter
-				TCCR0B &= pwm_stop;
+			if ( DDRB & output_start ) {
+				// PWM Output Stop
+				TCCR0A &= pwm_output_b_stop;
 				// PB1 (OC0B) Low
-				PORTB &= pwm_clear;
+				PORTB &= output_clear;
 				// Bit Value Clear PB1(OC0B), High-Z State
 				DDRB &= output_stop;
 			}

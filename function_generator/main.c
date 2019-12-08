d0f286c1f51869008aeec4786ebfbbba8538ecc2
Kenta Ishii <k.i.drivingroad21@gmail.com>
2019-12-08 11:19:08 +0900

    Modified Function Generator, Considered of Binary Size and Clock Consumption

--- function_generator/main.c
+++ function_generator/main.c
@@ -18,18 +18,26 @@
 /**
  * Output Sawtooth Wave from PB0 (OC0A)
  * Output Square Wave from PB1 (OC0B)
- * Input from PB4 (ADC2) to Determine Output Frequency
+ * Input from PB2 (ADC1) to Determine Output Frequency
+ * Input from PB4 (ADC2) to Calibrate Output Frequency, ADC Value 0 Means -15, ADC Value 255 Means +15
  * In default, the voltage vias is set as 0x80, the lowest peak is set as 0x01, the highest peak is set as 0xFF.
  */
 
 #define SAMPLE_RATE (F_CPU / 256) // 37500
-uint8_t const voltage_bias = 0x80;
-uint8_t const absolute_peak = 0x7F; // For Square Wave
-uint8_t osccal_default;
+#define VOLTAGE_BIAS 0x80
+#define ABSOLUTE_PEAK 0x7F // For Square Wave
+#define PEAK_HIGH (VOLTAGE_BIAS + ABSOLUTE_PEAK)
+#define PEAK_LOW (VOLTAGE_BIAS - ABSOLUTE_PEAK)
+#define PEAK_TO_PEAK (ABSOLUTE_PEAK * 2)
 
-uint16_t sample_count = 0; // 0-255
-uint16_t count_per_pi = 0; // Count per Pi (Approx. 3.14) Radian
-uint16_t count_per_2pi = 0; // Count per 2Pi Radian
+/* Global Variables without Initialization to Define at .bss Section and Squash .data Section */
+
+uint8_t osccal_default; // Calibrated Default Value of OSCCAL
+int8_t osccal_tuning; // Tuning Value for Variable Tone
+int8_t osccal_calibration; // Calibration Value from ADC2
+uint16_t sample_count; // 0-255
+uint16_t count_per_pi; // Count per Pi (Approx. 3.14) Radian
+uint16_t count_per_2pi; // Count per 2Pi Radian
 
 /**
  *                      SAMPLE_RATE
@@ -37,17 +45,21 @@ uint16_t count_per_2pi = 0; // Count per 2Pi Radian
  *                       Frequency
  */
 
-uint16_t delta_sawtooth = 0; // Fixed Point Arithmetic, Bit[15:12] Reserved for Calculation, Bit[11:4] UINT8, Bit[3:0] Fractional Part
+uint16_t fixed_value_sawtooth; // Fixed Point Arithmetic, Bit[15:12] Reserved for Calculation, Bit[11:4] UINT8, Bit[3:0] Fractional Part
+uint16_t fixed_delta_sawtooth; // Fixed Point Arithmetic, Bit[15:12] Reserved for Calculation, Bit[11:4] UINT8, Bit[3:0] Fractional Part
 
 /**
- *                    254 (Peak to Peak)
- * delta_sawtooth = ----------------------
- *                      count_per_2pi
+ *                         PEAK_TO_PEAK
+ * fixed_delta_sawtooth = ---------------
+ *                         count_per_2pi
  */
 
-uint8_t function_start = 0;
+uint8_t function_start;
 
 int main(void) {
+
+	/* Declare and Define Local Constants and Variables */
+
 	uint8_t const select_adc_channel_1 = _BV(MUX0); // ADC1 (PB2)
 	uint8_t const select_adc_channel_2 = _BV(MUX1); // ADC2 (PB4)
 	uint8_t const clear_adc_channel = ~(_BV(MUX1)|_BV(MUX0));
@@ -57,11 +69,25 @@ int main(void) {
 	uint8_t value_adc_channel_2_high_buffer = 0; // Bit[7:0] Is ADC[9:2]
 	uint8_t value_adc_channel_2_high = 0; // Bit[7:0] Is ADC[9:2]
 	uint16_t count_per_2pi_buffer = 0;
+	uint16_t fixed_delta_sawtooth_buffer = 0;
+	int8_t osccal_calibration_buffer = 0;
+
+	/* Initialize Global Variables */
+
+	osccal_default = OSCCAL;
+	osccal_tuning = 0;
+	osccal_calibration = 0;
+	sample_count = 0;
+	count_per_pi = 0;
+	count_per_2pi = 0;
+	fixed_value_sawtooth = 0;
+	fixed_delta_sawtooth = 0;
+	function_start = 0;
 
 	/* Clock Calibration */
 
-	OSCCAL += 0x03; // Frequency Calibration for Individual Difference at VCC = 3.3V
-	osccal_default = OSCCAL;
+	osccal_default += 0x03; // Frequency Calibration for Individual Difference at VCC = 3.3V
+	OSCCAL = osccal_default;
 
 	/* I/O Settings */
 
@@ -86,10 +112,10 @@ int main(void) {
 	TCNT0 = 0;
 
 	// Set Output Compare A
-	OCR0A = voltage_bias;
+	OCR0A = VOLTAGE_BIAS;
 
 	// Set Output Compare B
-	OCR0B = voltage_bias;
+	OCR0B = VOLTAGE_BIAS;
 
 	// Set Timer/Counter0 Overflow Interrupt for "ISR(TIM0_OVF_vect)"
 	TIMSK0 = _BV(TOIE0);
@@ -109,7 +135,55 @@ int main(void) {
 		value_adc_channel_1_high_buffer = ADCH; // ADC[9:0] Will Be Updated After High Bits Are Read
 		if ( value_adc_channel_1_high_buffer != value_adc_channel_1_high ) {
 			value_adc_channel_1_high = value_adc_channel_1_high_buffer;
-			OSCCAL = osccal_default + ((int8_t)(0x80^(value_adc_channel_1_high)) >> 4); // Arithmetic Logical Shift Right
+			if ( value_adc_channel_1_high >= 224 ) {
+				count_per_2pi_buffer = 39; // Count per 2Pi Radian, 937.5 Hz
+				osccal_tuning = 0;
+			} else if ( value_adc_channel_1_high >= 192 ) {
+				count_per_2pi_buffer = 59; // Count per 2Pi Radian, 625 Hz
+				osccal_tuning = 0;
+			} else if ( value_adc_channel_1_high >= 160 ) {
+				count_per_2pi_buffer = 79; // Count per 2Pi Radian, 468.75 Hz
+				osccal_tuning = 0;
+			} else if ( value_adc_channel_1_high >= 128 ) {
+				count_per_2pi_buffer = 99; // Count per 2Pi Radian, 375 Hz
+				osccal_tuning = 0;
+			} else if ( value_adc_channel_1_high >= 96 ) {
+				count_per_2pi_buffer = 119; // Count per 2Pi Radian, Approx. 312.5 Hz
+				osccal_tuning = 0;
+			} else if ( value_adc_channel_1_high >= 64 ) {
+				count_per_2pi_buffer = 139; // Count per 2Pi Radian, Approx. 267.86 Hz
+				osccal_tuning = 0;
+			} else if ( value_adc_channel_1_high >= 32 ) {
+				count_per_2pi_buffer = 159; // Count per 2Pi Radian, 234.375 Hz
+				osccal_tuning = 0;
+			} else { // ADC Value < 32
+				count_per_2pi_buffer = 0;
+				osccal_tuning = 0;
+			}
+			if ( count_per_2pi_buffer != count_per_2pi ) {
+				if ( count_per_2pi_buffer ) {
+					// Fixed Point Arithmetic (DIV), LSL4 to Dividend
+					fixed_delta_sawtooth_buffer = ((PEAK_TO_PEAK << 4) << 4) / (count_per_2pi_buffer << 4);
+					cli(); // Stop to Issue Interrupt
+					sample_count = 0;
+					count_per_2pi = count_per_2pi_buffer;
+					count_per_pi = count_per_2pi >> 1;
+					fixed_delta_sawtooth = fixed_delta_sawtooth_buffer;
+					function_start = 1;
+					OSCCAL = osccal_default + osccal_tuning + osccal_calibration;
+					sei(); // Start to Issue Interrupt
+				} else {
+					fixed_delta_sawtooth_buffer = 0;
+					cli(); // Stop to Issue Interrupt
+					sample_count = 0;
+					count_per_2pi = 0;
+					count_per_pi = 0;
+					fixed_delta_sawtooth = 0;
+					function_start = 0;
+					OSCCAL = osccal_default + osccal_tuning + osccal_calibration;
+					sei(); // Start to Issue Interrupt
+				}
+			}
 		}
 
 		ADMUX |= select_adc_channel_2;
@@ -119,41 +193,11 @@ int main(void) {
 		value_adc_channel_2_high_buffer = ADCH; // ADC[9:0] Will Be Updated After High Bits Are Read
 		if ( value_adc_channel_2_high_buffer != value_adc_channel_2_high ) {
 			value_adc_channel_2_high = value_adc_channel_2_high_buffer;
-			if ( value_adc_channel_2_high >= 224 ) {
-				count_per_2pi_buffer = 39; // Count per 2Pi Radian, 937.5 Hz
-			} else if ( value_adc_channel_2_high >= 192 ) {
-				count_per_2pi_buffer = 59; // Count per 2Pi Radian, 625 Hz
-			} else if ( value_adc_channel_2_high >= 160 ) {
-				count_per_2pi_buffer = 79; // Count per 2Pi Radian, 468.75 Hz
-			} else if ( value_adc_channel_2_high >= 128 ) {
-				count_per_2pi_buffer = 99; // Count per 2Pi Radian, 375 Hz
-			} else if ( value_adc_channel_2_high >= 96 ) {
-				count_per_2pi_buffer = 119; // Count per 2Pi Radian, Approx. 312.5 Hz
-			} else if ( value_adc_channel_2_high >= 64 ) {
-				count_per_2pi_buffer = 139; // Count per 2Pi Radian, Approx. 267.86 Hz
-			} else if ( value_adc_channel_2_high >= 32 ) {
-				count_per_2pi_buffer = 159; // Count per 2Pi Radian, 234.375 Hz
-			} else { // ADC Value < 32
-				count_per_2pi_buffer = 0;
-			}
-			if ( count_per_2pi_buffer != count_per_2pi ) {
-				if ( count_per_2pi_buffer ) {
-					cli(); // Stop to Issue Interrupt
-					sample_count = 0;
-					count_per_2pi = count_per_2pi_buffer;
-					count_per_pi = count_per_2pi >> 1;
-					delta_sawtooth = (254 << 8) / (count_per_2pi << 4); // Fixed Point Arithmetic (LSL4 to Dividend)
-					function_start = 1;
-					sei(); // Start to Issue Interrupt
-				} else {
-					cli(); // Stop to Issue Interrupt
-					sample_count = 0;
-					count_per_2pi = 0;
-					count_per_pi = 0;
-					delta_sawtooth = 0; // Fixed Point Arithmetic (LSL4 to Dividend)
-					function_start = 0;
-					sei(); // Start to Issue Interrupt
-				}
+			// Convert (0)-(255) to (-127)-(127) by EOR with 0x80, Arithmetic Logical Shift Right for Range (-15)-(15)
+			osccal_calibration_buffer = ((int8_t)(0x80^(value_adc_channel_2_high)) >> 3);
+			if ( osccal_calibration_buffer !=  osccal_calibration ) {
+				osccal_calibration = osccal_calibration_buffer;
+				OSCCAL = osccal_default + osccal_tuning + osccal_calibration;
 			}
 		}
 		_delay_ms(2);
@@ -162,32 +206,39 @@ int main(void) {
 }
 
 ISR(TIM0_OVF_vect) {
-	uint16_t temp1;
-	uint16_t temp2;
+	uint16_t temp;
 
 	if ( function_start ) { // Start Function
 		// Saw Tooth Wave
-		if ( sample_count < count_per_2pi ) {
+		if ( sample_count == 0 ) {
+			OCR0A = PEAK_LOW;
+			fixed_value_sawtooth = PEAK_LOW << 4;
+		} else if ( sample_count < count_per_2pi ) {
+			/* Equivalence of */
 			// Prepare for Fixed Point Arithmetic, Bit[11:4] UINT8, Bit[3:0] Fractional Part
-			temp1 = sample_count << 4;
-			// Fixed Point Arithmetic, Logical Shift Right 8 Times to Make Bit[7:0] UINT8
-			temp2 = (delta_sawtooth * temp1) >> 8;
-			temp2 += 1; // Lowest Peak 0x01
-			if ( temp2 > 0xFF ) temp2 = 0xFF; // Saturate at 8-bit
-			OCR0A = temp2;
+			//temp = sample_count << 4;
+			// Fixed Point Arithmetic (MUL), Logical Shift Right 4 Times to Make Bit[7:0] UINT8
+			//temp = ((fixed_delta_sawtooth * temp) >> 4 ) >> 4;
+			//temp += PEAK_LOW;
+			/* And */
+			fixed_value_sawtooth += fixed_delta_sawtooth; // Fixed Point Arithmetic (ADD)
+			temp = fixed_value_sawtooth >> 4; // Make Bit[7:0] UINT8
+			/* End of Equivalence */
+			if ( temp > 0xFF ) temp = 0xFF; // Saturate at 8-bit
+			OCR0A = temp;
 		} else {
-			OCR0A = voltage_bias + absolute_peak;
+			OCR0A = PEAK_HIGH;
 		}
 		// Square Wave
 		if ( sample_count <= count_per_pi ) {
-			OCR0B = voltage_bias + absolute_peak;
+			OCR0B = PEAK_HIGH;
 		} else {
-			OCR0B = voltage_bias - absolute_peak;
+			OCR0B = PEAK_LOW;
 		}
 		sample_count++;
 		if ( sample_count > count_per_2pi ) sample_count = 0;
 	} else { // Stop Function
-		OCR0A = voltage_bias;
-		OCR0B = voltage_bias;
+		OCR0A = VOLTAGE_BIAS;
+		OCR0B = VOLTAGE_BIAS;
 	}
 }

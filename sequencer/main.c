89b84ab49c9e76c8739404382b521228d31b17e5
Kenta Ishii <k.i.drivingroad21@gmail.com>
2019-12-20 20:21:53 +0900

    Modified Sequencer 1. 3 Sequences 2. Length of Sequence

--- sequencer/main.c
+++ sequencer/main.c
@@ -12,6 +12,7 @@
 #include <avr/io.h>
 #include <avr/cpufunc.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 #include <util/delay.h>
 #include <util/delay_basic.h>
 
@@ -19,10 +20,13 @@
 
 /**
  * Output Sawtooth Wave from PB0 (OC0A)
- * Input from PB2 (Button 1), Start Sequencer If Detected Low
- * Input from PB3 (Button 2), Start Sequencer If Detected Low
- * If either Button 1 or Button 2 detects low, Sequence no.1 starts.
- * If both Button 1 and Button 2 detect low, Sequence no.2 starts.
+ * Input from PB2 (Bit[0]), Set by Detecting Low
+ * Input from PB3 (Bit[1]), Set by Detecting Low
+ * Bit[1:0]:
+ *     0b00: Stop Sequencer
+ *     0b01: Play Sequence No.1
+ *     0b10: Play Sequence No.2
+ *     0b11: PLay Sequence No.3
  * Note: The wave may not reach the high peak, 0xFF (255) in default,
  *       because of its low precision decimal system.
  */
@@ -32,15 +36,12 @@
 #define PEAK_HIGH 0xFF
 #define PEAK_TO_PEAK (PEAK_HIGH - PEAK_LOW)
 #define SEQUENCER_INTERVAL 9375 // 4Hz = 0.25 Seconds
-#define SEQUENCER_COUNTUPTO 10 // 0.25 Seconds * 10
+#define SEQUENCER_COUNTUPTO 16 // 0.25 Seconds * 16
 
 /* Global Variables without Initialization to Define at .bss Section and Squash .data Section */
 
-uint8_t osccal_default; // Calibrated Default Value of OSCCAL
-int8_t osccal_tuning; // Tuning Value for Variable Tone
-uint16_t sample_count; // 0-255
-uint16_t count_per_pi; // Count per Pi (Approx. 3.14) Radian
-uint16_t count_per_2pi; // Count per 2Pi Radian
+uint16_t sample_count; // Count per Timer/Counter0 Overflow Interrupt
+uint16_t count_per_2pi; // Count Number for 2Pi Radian to Make Wave
 
 /**
  *                      SAMPLE_RATE
@@ -60,14 +61,14 @@ uint16_t fixed_delta_sawtooth; // Fixed Point Arithmetic, Bit[15] Sign, Bit[14:7
 uint8_t function_start;
 uint16_t sequencer_interval_count;
 uint16_t sequencer_count_update;
-uint16_t sequencer_count_last;
 
 /**
  * Bit[7:0]: 0-255 Tone Select
  */
-uint8_t sequencer_array[2][SEQUENCER_COUNTUPTO] = {
-	{1,3,3,4,4,5,5,5,6,7},
-	{0,1,2,2,0,3,4,4,0,5}
+uint8_t const sequencer_array[3][SEQUENCER_COUNTUPTO] PROGMEM = { // Array in Program Space
+	{ 1, 3, 3, 4, 4, 5, 5, 5, 6, 7, 1, 2, 3, 4, 5, 6}, // Sequence No.1
+	{ 0, 3, 3, 5, 5, 7, 7, 9, 9,10,10,10, 5, 5, 3, 3}, // Sequence No.2
+	{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 9, 8, 7, 6, 5}  // Sequence No.3
 };
 
 int main(void) {
@@ -77,21 +78,22 @@ int main(void) {
 	uint8_t const pin_button2 = _BV(PINB3); // Assign PB3 as Button Input
 	uint16_t count_per_2pi_buffer = 0;
 	uint16_t fixed_delta_sawtooth_buffer = 0;
+	uint16_t sequencer_count_last = 0;
 	uint8_t sequencer_value;
 	uint8_t input_pin;
+	uint8_t osccal_default; // Calibrated Default Value of OSCCAL
+	int8_t osccal_tuning; // Tuning Value for Variable Tone
 
 	/* Initialize Global Variables */
 
 	osccal_default = OSCCAL;
 	osccal_tuning = 0;
 	sample_count = 0;
-	count_per_pi = 0;
 	count_per_2pi = 0;
 	fixed_value_sawtooth = 0;
 	fixed_delta_sawtooth = 0;
 	sequencer_interval_count = 0;
 	sequencer_count_update = 0;
-	sequencer_count_last = 0;
 
 	/* Clock Calibration */
 
@@ -125,39 +127,51 @@ int main(void) {
 	while(1) {
 		input_pin = 0;
 		if ( ! (PINB & pin_button1) ) {
-			input_pin += 0b01;
+			input_pin |= 0b01;
 		}
 		if ( ! (PINB & pin_button2) ) {
-			input_pin += 0b01;
+			input_pin |= 0b10;
 		}
 		if ( input_pin ) {
 			if ( ! sequencer_count_update || sequencer_count_update != sequencer_count_last ) {
 				if ( ! sequencer_count_update ) sequencer_count_update = 1; // If Zero, Not Starting
-				sequencer_value = sequencer_array[input_pin - 1][(sequencer_count_update - 1)];
-				/* Pentatonic Scale, A Minor and C Major, 37500 Samples per Seconds */
-				if ( sequencer_value == 7 ) {
+				sequencer_value = pgm_read_byte(&(sequencer_array[input_pin - 1][(sequencer_count_update - 1)]));
+				/* Heptatonic Scale, A Minor and C Major, 37500 Samples per Seconds */
+				if ( sequencer_value == 10 ) {
 					count_per_2pi_buffer = 35; // C6 1046.50 Hz
 					fixed_delta_sawtooth_buffer = 7<<7|0b0100100;
 					osccal_tuning = 1;
-				} else if ( sequencer_value == 6 ) {
+				} else if ( sequencer_value == 9 ) {
+					count_per_2pi_buffer = 37; // B5 987.77 Hz
+					fixed_delta_sawtooth_buffer = 6<<7|0b1110010;
+					osccal_tuning = 0;
+				} else if ( sequencer_value == 8 ) {
 					count_per_2pi_buffer = 41; // A5 880.00 Hz
 					fixed_delta_sawtooth_buffer = 6<<7|0b0011100;
 					osccal_tuning = -1;
-				} else if ( sequencer_value == 5 ) {
+				} else if ( sequencer_value == 7 ) {
 					count_per_2pi_buffer = 47; // G5 783.99 Hz
 					fixed_delta_sawtooth_buffer = 5<<7|0b0110110;
 					osccal_tuning = 1;
-				} else if ( sequencer_value == 4 ) {
-					count_per_2pi_buffer = 56; // E 659.26
+				} else if ( sequencer_value == 6 ) {
+					count_per_2pi_buffer = 52; // F5 698.46
+					fixed_delta_sawtooth_buffer = 4<<7|0b1110011;
+					osccal_tuning = 1;
+				} else if ( sequencer_value == 5 ) {
+					count_per_2pi_buffer = 56; // E5 659.26
 					fixed_delta_sawtooth_buffer = 4<<7|0b1000110;
 					osccal_tuning = 1;
-				} else if ( sequencer_value == 3 ) {
+				} else if ( sequencer_value == 4 ) {
 					count_per_2pi_buffer = 63; // D5 587.33 Hz
 					fixed_delta_sawtooth_buffer = 4<<7|0b0000110;
 					osccal_tuning = 1;
-				} else if ( sequencer_value == 2 ) {
+				} else if ( sequencer_value == 3 ) {
 					count_per_2pi_buffer = 70; // C5 523.25 Hz
 					fixed_delta_sawtooth_buffer = 3<<7|0b1010010;
+					osccal_tuning = 0;
+				} else if ( sequencer_value == 2 ) {
+					count_per_2pi_buffer = 75; // B4 493.88 Hz
+					fixed_delta_sawtooth_buffer = 3<<7|0b0110011;
 					osccal_tuning = 0;
 				} else if ( sequencer_value == 1 ) {
 					count_per_2pi_buffer = 84; // A4 440.00 Hz
@@ -173,7 +187,6 @@ int main(void) {
 						cli(); // Stop to Issue Interrupt
 						sample_count = 0;
 						count_per_2pi = count_per_2pi_buffer;
-						count_per_pi = count_per_2pi >> 1;
 						fixed_delta_sawtooth = fixed_delta_sawtooth_buffer;
 						function_start = 1;
 						sei(); // Start to Issue Interrupt
@@ -181,7 +194,6 @@ int main(void) {
 						cli(); // Stop to Issue Interrupt
 						sample_count = 0;
 						count_per_2pi = 0;
-						count_per_pi = 0;
 						fixed_delta_sawtooth = 0;
 						function_start = 0;
 						sei(); // Start to Issue Interrupt
@@ -197,7 +209,6 @@ int main(void) {
 			cli(); // Stop to Issue Interrupt
 			sample_count = 0;
 			count_per_2pi = 0;
-			count_per_pi = 0;
 			fixed_delta_sawtooth = 0;
 			function_start = 0;
 			sei(); // Start to Issue Interrupt
